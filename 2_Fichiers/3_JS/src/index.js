1271c2e7533ffa00b7cc7731446563ea59be6a89
AlexandreBasei <alexandreacevedobasei@gmail.com>
2023-01-13 16:03:18 +0100

    ADD troiseme graphe

--- 2_Fichiers/3_JS/src/index.js
+++ 2_Fichiers/3_JS/src/index.js
@@ -101,20 +101,145 @@ document.addEventListener('DOMContentLoaded', function () {
 	var ranking = [];
 	var labels = [];
 	var chart = '';
-
-	$.ajax({
+	
+	var settings1 = {
 		"async": true,
 		"crossDomain": true,
 		// "url": "https://anime-db.p.rapidapi.com/anime?page=1&size=10",
-		"url": "https://anime-db.p.rapidapi.com/anime?page=1&size=10&genres=" + url,
+		"url": "https://anime-db.p.rapidapi.com/anime?page=1&size=10",
 		"method": "GET",
 		"headers": {
 			"X-RapidAPI-Key": "d8c4835dbfmshe28f6af9702c0b2p1441d5jsnaeb2ffc66cfa",
 			// "X-RapidAPI-Key": "413494542fmsh37e255b3fc7392cp19f59ejsn92264c7d882c",
-			"X-RapidAPI-Host": "anime-db.p.rapidapi.com",
-		},
-		success: function (response) {
+			"X-RapidAPI-Host": "anime-db.p.rapidapi.com"
+		}
+	}
 
+	$.ajax(settings1).done(function (response) {
+		console.log(response);
+
+		ranking = [];
+		labels = [];
+
+		response['data'].forEach(element => {
+			ranking.push(element['ranking']);
+		});
+
+		response['data'].forEach(element => {
+			labels.push(element['title']);
+		});
+
+		chart = new Chart(Graphe1, {
+			type: 'bar',
+			data: {
+				labels: labels,
+				color: 'white',
+				datasets: [
+					{
+						label: 'Rang',
+						data: ranking,
+						backgroundColor: ["#a2d2ff", "red", "green", "gold", "purple", "hotpink", "brown", "blue", "orange", "blueviolet"],
+						borderWidth: 0,
+						borderRadius: 20,
+						color: 'white',
+					},
+				],
+			},
+			options: {
+				// indexAxis: 'y',
+				legend: {
+					display: false,
+				},
+				scales: {
+					x: {
+						ticks: [{
+							color: 'white',
+
+						}],
+						grid: {
+							color: 'whitesmoke',
+							borderColor: 'whitesmoke'
+						}
+					},
+					y: {
+						// beginAtZero: true,
+						// reverse: true,
+						ticks: {
+							color: 'white',
+							reverse: true,
+						},
+						grid: {
+							color: 'whitesmoke',
+							borderColor: 'whitesmoke'
+						}
+					},
+				},
+			},
+		});
+		chart.update();
+	});
+
+	$("#genre").on("change", function () {
+		Opgenre = select.value;
+
+		if (Opgenre == 'Top') {
+			url = '';
+		}
+
+		if (Opgenre == 'Action') {
+			url = '&genres=Action';
+		}
+		if (Opgenre == 'Suspense') {
+			url = '&genres=Suspense';
+		}
+		if (Opgenre == 'Horror') {
+			url = '&genres=Horror';
+		}
+		if (Opgenre == 'Sports') {
+			url = '&genres=Sports';
+		}
+		if (Opgenre == 'Supernatural') {
+			url = '&genres=Supernatural';
+		}
+		if (Opgenre == 'Fantasy') {
+			url = '&genres=Fantasy';
+		}
+		if (Opgenre == 'Drama') {
+			url = '&genres=Drama';
+		}
+		if (Opgenre == 'Comedy') {
+			url = '&genres=Comedy';
+		}
+		if (Opgenre == 'Mystery') {
+			url = '&genres=Mystery';
+		}
+		if (Opgenre == 'Slice of Life') {
+			url = '&genres=Slice%20of%20Life';
+		}
+		if (Opgenre == 'Adventure') {
+			url = '&genres=Adventure';
+		}
+		if (Opgenre == 'Romance') {
+			url = '&genres=Romance';
+		}
+		if (Opgenre == 'Sci-Fi') {
+			url = '&genres=Sci-Fi';
+		}
+
+		settings1 = {
+			"async": true,
+			"crossDomain": true,
+			// "url": "https://anime-db.p.rapidapi.com/anime?page=1&size=10",
+			"url": "https://anime-db.p.rapidapi.com/anime?page=1&size=10" + url,
+			"method": "GET",
+			"headers": {
+				"X-RapidAPI-Key": "d8c4835dbfmshe28f6af9702c0b2p1441d5jsnaeb2ffc66cfa",
+				// "X-RapidAPI-Key": "413494542fmsh37e255b3fc7392cp19f59ejsn92264c7d882c",
+				"X-RapidAPI-Host": "anime-db.p.rapidapi.com"
+			}
+		}
+
+		$.ajax(settings1).done(function (response) {
 			ranking = [];
 			labels = [];
 
@@ -126,130 +251,9 @@ document.addEventListener('DOMContentLoaded', function () {
 				labels.push(element['title']);
 			});
 
-			chart = new Chart(Graphe1, {
-				type: 'bar',
-				data: {
-					labels: labels,
-					color: 'white',
-					datasets: [
-						{
-							label: 'Rang',
-							data: ranking,
-							backgroundColor: ["#a2d2ff", "red", "green", "gold", "purple", "hotpink", "brown", "blue", "orange", "blueviolet"],
-							borderWidth: 0,
-							borderRadius: 20,
-							color: 'white',
-						},
-					],
-				},
-				options: {
-					// indexAxis: 'y',
-					legend: {
-						display: false,
-					},
-					scales: {
-						x: {
-							ticks: [{
-								color: 'white',
-
-							}],
-							grid: {
-								color: 'whitesmoke',
-								borderColor: 'whitesmoke'
-							}
-						},
-						y: {
-							// beginAtZero: true,
-							// reverse: true,
-							ticks: {
-								color: 'white',
-								reverse: true,
-							},
-							grid: {
-								color: 'whitesmoke',
-								borderColor: 'whitesmoke'
-							}
-						},
-					},
-				},
-			});
+			chart.data.labels = labels;
+			chart.data.datasets[0].data = ranking;
 			chart.update();
-		}
-	});
-
-	$("#genre").on("change", function () {
-		Opgenre = select.value;
-
-		if (Opgenre == 'Action') {
-			url = 'Action';
-		}
-		if (Opgenre == 'Suspense') {
-			url = 'Suspense';
-		}
-		if (Opgenre == 'Horror') {
-			url = 'Horror';
-		}
-		if (Opgenre == 'Sports') {
-			url = 'Sports';
-		}
-		if (Opgenre == 'Supernatural') {
-			url = 'Supernatural';
-		}
-		if (Opgenre == 'Fantasy') {
-			url = 'Fantasy';
-		}
-		if (Opgenre == 'Drama') {
-			url = 'Drama';
-		}
-		if (Opgenre == 'Comedy') {
-			url = 'Comedy';
-		}
-		if (Opgenre == 'Mystery') {
-			url = 'Mystery';
-		}
-		if (Opgenre == 'Slice of Life') {
-			url = 'Slice%20of%20Life';
-		}
-		if (Opgenre == 'Adventure') {
-			url = 'Adventure';
-		}
-		if (Opgenre == 'Romance') {
-			url = 'Romance';
-		}
-		if (Opgenre == 'Sci-Fi') {
-			url = 'Sci-Fi';
-		}
-		if (Opgenre == 'Fantasy') {
-			url = 'Fantasy';
-		}
-
-		$.ajax({
-			"async": true,
-			"crossDomain": true,
-			// "url": "https://anime-db.p.rapidapi.com/anime?page=1&size=10",
-			"url": "https://anime-db.p.rapidapi.com/anime?page=1&size=10&genres=" + url,
-			"method": "GET",
-			"headers": {
-				"X-RapidAPI-Key": "d8c4835dbfmshe28f6af9702c0b2p1441d5jsnaeb2ffc66cfa",
-				// "X-RapidAPI-Key": "413494542fmsh37e255b3fc7392cp19f59ejsn92264c7d882c",
-				"X-RapidAPI-Host": "anime-db.p.rapidapi.com",
-			},
-			success: function (response) {
-				ranking = [];
-				labels = [];
-
-				response['data'].forEach(element => {
-					ranking.push(element['ranking']);
-				});
-
-				response['data'].forEach(element => {
-					labels.push(element['title']);
-				});
-
-				chart.data.labels = labels;
-				chart.data.datasets[0].data = ranking;
-				chart.update();
-			}
 		});
 	})
 
@@ -279,6 +283,20 @@ document.addEventListener('DOMContentLoaded', function () {
 	var romance = 0;
 	var science = 0;
 
+	var action2 = 0;
+	var suspense2 = 0;
+	var horreur2 = 0;
+	var sport2 = 0;
+	var surnaturel2 = 0;
+	var fantasy2 = 0;
+	var drama2 = 0;
+	var comedie2 = 0;
+	var mystere2 = 0;
+	var tranche2 = 0;
+	var aventure2 = 0;
+	var romance2 = 0;
+	var science2 = 0;
+
 	const genre = ["Action", "Suspense", "Horreur", "Sports", "Surnaturel", "Fantasy", "Drame", "Comédie", "Mystère", "Tranche de vie", "Aventure", "Romance", "Sci-Fi"];
 
 	$.ajax(settings2).done(function (response) {
@@ -292,42 +310,55 @@ document.addEventListener('DOMContentLoaded', function () {
 
 					if (element2 == 'Action') {
 						action++;
+						action2 += element['episodes'];
 					}
 					if (element2 == 'Suspense') {
 						suspense++;
+						suspense2 += element['episodes'];
 					}
 					if (element2 == 'Horror') {
 						horreur++;
+						horreur2 += element['episodes'];
 					}
 					if (element2 == 'Sports') {
 						sport++;
+						sport2 += element['episodes'];
 					}
 					if (element2 == 'Supernatural') {
 						surnaturel++;
+						surnaturel2 += element['episodes'];
 					}
 					if (element2 == 'Fantasy') {
 						fantasy++;
+						fantasy2 += element['episodes'];
 					}
 					if (element2 == 'Drama') {
 						drama++;
+						drama2 += element['episodes'];
 					}
 					if (element2 == 'Comedy') {
 						comedie++;
+						comedie2 += element['episodes'];
 					}
 					if (element2 == 'Mystery') {
 						mystere++;
+						mystere2 += element['episodes'];
 					}
 					if (element2 == 'Slice of Life') {
 						tranche++;
+						tranche2 += element['episodes'];
 					}
 					if (element2 == 'Adventure') {
 						aventure++;
+						aventure2 += element['episodes'];
 					}
 					if (element2 == 'Romance') {
 						romance++;
+						romance2 += element['episodes'];
 					}
 					if (element2 == 'Sci-Fi') {
 						science++;
+						science2 += element['episodes'];
 					}
 				});
 			});
@@ -377,61 +408,61 @@ document.addEventListener('DOMContentLoaded', function () {
 			},
 		});
 		chart2.update();
-	});
 
-	var chart3 = new Chart(Graphe3, {
-		type: 'radar',
-		data: {
-			labels: ["Action", "Suspense", "Horreur", "Sports", "Surnaturel", "Fantasy", "Drame", "Comédie", "Mystère", "Tranche de vie", "Aventure", "Romance", "Sci-Fi"],
-			datasets: [{
-				label: 'Note moyenne ',
-				data: [65, 59, 90, 81, 56, 55, 40, 56, 12, 60, 82, 57, 48],
-				fill: true,
-				backgroundColor: 'rgba(255, 99, 132, 0.2)',
-				borderColor: 'rgb(255, 99, 132)',
-				pointBackgroundColor: ['rgb(255, 99, 132)', 'blue', 'yellow', 'green', 'gold', 'silver', 'purple', 'cyan', 'orange', 'blueviolet', 'hotpink', 'bisque', 'brown'],
-				pointBorderColor: '#fff',
-				pointHoverBackgroundColor: '#fff',
-				pointHoverBorderColor: 'rgb(255, 99, 132)',
-			}]
-		},
-		options: {
-			color: 'white',
-			scales: {
-				r: {
-					angleLines: {
-						color: 'CadetBlue'
+		var chart3 = new Chart(Graphe3, {
+			type: 'radar',
+			data: {
+				labels: ["Action", "Suspense", "Horreur", "Sports", "Surnaturel", "Fantasy", "Drame", "Comédie", "Mystère", "Tranche de vie", "Aventure", "Romance", "Sci-Fi"],
+				datasets: [{
+					label: "Nombre d'épisodes ",
+					data: [action2, suspense2, horreur2, sport2, surnaturel2, fantasy2, drama2, comedie2, mystere2, tranche2, aventure2, romance2, science2],
+					fill: true,
+					backgroundColor: 'rgba(255, 99, 132, 0.2)',
+					borderColor: 'rgb(255, 99, 132)',
+					pointBackgroundColor: ['rgb(255, 99, 132)', 'blue', 'yellow', 'green', 'gold', 'silver', 'purple', 'cyan', 'orange', 'blueviolet', 'hotpink', 'bisque', 'brown'],
+					pointBorderColor: '#fff',
+					pointHoverBackgroundColor: '#fff',
+					pointHoverBorderColor: 'rgb(255, 99, 132)',
+				}]
+			},
+			options: {
+				color: 'white',
+				scales: {
+					r: {
+						angleLines: {
+							color: 'CadetBlue'
+						},
+						grid: {
+							color: 'CadetBlue',
+							opacity: 0.5
+						},
+						ticks: {
+							color: 'white',
+							backdropColor: 'transparent'
+							// display: false
+						},
+						pointLabels: {
+							color: 'white'
+						}
 					},
-					grid: {
-						color: 'CadetBlue',
-						opacity: 0.5
+					y: {
+						ticks: {
+							display: false
+						}
 					},
-					ticks: {
-						color: 'white',
-						backdropColor: 'transparent'
-						// display: false
-					},
-					pointLabels: {
+				},
+				elements: {
+					line: {
+						borderWidth: 3,
+					}
+				},
+				plugins: {
+					labels: {
 						color: 'white'
 					}
-				},
-				y: {
-					ticks: {
-						display: false
-					}
-				},
-			},
-			elements: {
-				line: {
-					borderWidth: 3,
 				}
 			},
-			plugins: {
-				labels: {
-					color: 'white'
-				}
-			}
-		},
+		});
+		chart3.update();
 	});
-	chart3.update();
 });
